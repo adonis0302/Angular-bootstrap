ng-bootstrap/ng-bootstrap

feat(datepicker): add keyboard navigation
The following shortcuts are available:
Arrow left/right: previous/next day
Arrow up/down: previous/next week
Page up/down: previous/next month
Shift+page up/down: previous/next year
Home/end: beginning/end of the current view
Shift+home/end: min/max selectable date

src/datepicker/datepicker-month-view.ts
@@ -43,7 +43,8 @@ import {DayTemplateContext} from './datepicker-day-template-context';
           [ngOutletContext]="{date: {year: day.date.year, month: day.date.month, day: day.date.day},
             currentMonth: month.number,
             disabled: isDisabled(day),
-            selected: isSelected(day.date)}">
+            selected: isSelected(day.date),
+            focused: isFocused(day.date)}">
           </template>
       </div>
     </div>
@@ -55,6 +56,7 @@ export class NgbDatepickerMonthView {
   @Input() month: MonthViewModel;
   @Input() outsideDays: 'visible' | 'hidden' | 'collapsed';
   @Input() selectedDate: NgbDate;
+  @Input() focusedDate: NgbDate;
   @Input() showWeekdays;
   @Input() showWeekNumbers;
 
@@ -70,7 +72,11 @@ export class NgbDatepickerMonthView {
 
   isDisabled(day: DayViewModel) { return this.disabled || day.disabled; }
 
-  isSelected(date: NgbDate) { return this.selectedDate && this.selectedDate.equals(date); }
+  isSelected(date: NgbDate) { return !!(this.selectedDate && this.selectedDate.equals(date)); }
+
+  isFocused(date: NgbDate) {
+    return !!(this.focusedDate && this.focusedDate.equals(date) && this.month.number === date.month);
+  }
 
   isCollapsed(day: DayViewModel) { return this.outsideDays === 'collapsed' && this.month.number !== day.date.month; }
 

adonis0302/Angular-bootstrap

src/datepicker/datepicker-month-view.ts
@@ -13,7 +13,7 @@ import {DayTemplateContext} from './datepicker-day-template-context';
     }
     .ngb-dp-day, .ngb-dp-weekday, .ngb-dp-week-number {
       width: 2rem;
-      height: 2rem;      
+      height: 2rem;
     }
     .ngb-dp-day {
       cursor: pointer;
@@ -37,6 +37,10 @@ import {DayTemplateContext} from './datepicker-day-template-context';
           <ng-template [ngIf]="!isHidden(day)">
             <ng-template [ngTemplateOutlet]="dayTemplate"
             [ngOutletContext]="_getDayContext(day, month)">
+              currentMonth: month.number,
+              disabled: isDisabled(day),
+              focused: isFocused(day.date),
+              selected: isSelected(day.date)}">
             </ng-template>
           </ng-template>
         </div>
@@ -47,6 +51,7 @@ import {DayTemplateContext} from './datepicker-day-template-context';
 export class NgbDatepickerMonthView {
   @Input() dayTemplate: TemplateRef<DayTemplateContext>;
   @Input() disabled: boolean;
+  @Input() focusedDate: NgbDate;
   @Input() month: MonthViewModel;
   @Input() outsideDays: 'visible' | 'hidden' | 'collapsed';
   @Input() selectedDate: NgbDate;
@@ -72,16 +77,20 @@ export class NgbDatepickerMonthView {
     };
   }
 
-  isDisabled(day: DayViewModel) { return this.disabled || day.disabled; }
-
-  isSelected(date: NgbDate) { return this.selectedDate && this.selectedDate.equals(date); }
-
   isCollapsed(week: WeekViewModel) {
     return this.outsideDays === 'collapsed' && week.days[0].date.month !== this.month.number &&
         week.days[week.days.length - 1].date.month !== this.month.number;
   }
 
+  isDisabled(day: DayViewModel) { return this.disabled || day.disabled; }
+
+  isFocused(date: NgbDate) {
+    return !!(this.focusedDate && this.focusedDate.equals(date) && this.month.number === date.month);
+  }
+
   isHidden(day: DayViewModel) {
     return (this.outsideDays === 'hidden' || this.outsideDays === 'collapsed') && this.month.number !== day.date.month;
   }
+
+  isSelected(date: NgbDate) { return !!(this.selectedDate && this.selectedDate.equals(date)); }
 }